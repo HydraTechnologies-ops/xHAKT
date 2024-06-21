955de267981b466009ebfaac8ec18fceb15bb702
GammaWraith <kahlob.bookout06@gmail.com>
2024-06-21 14:14:07 -0500

    commit to main

--- frontend/xhakt/hardhat.config.cjs
+++ frontend/xhakt/hardhat.config.cjs
@@ -1,12 +1,15 @@
 require("@nomicfoundation/hardhat-toolbox");
+require("@nomiclabs/hardhat-ethers");
+require("@openzeppelin/hardhat-upgrades");
 require("dotenv/config");
 
+const alchemyAPI = process.env.ALCHEMY_API_KEY_ETH;
 module.exports = {
-  solidity: "^0.8.20",
+  solidity: "0.8.20",
   networks: {
-    testnet: {
-      url: process.env.TESTNET_ENDPOINT,
-      accounts: [process.env.TESTNET_OPERATOR_PRIVATE_KEY],
+    sepolia: {
+      url: `https://eth-sepolia.g.alchemy.com/v2/${alchemyAPI}`,
+      accounts: [process.env.LOCAL_PRIVATE_KEY],
     },
   },
 };
